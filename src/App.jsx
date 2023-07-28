a1ed56a75765fdbd6294165875b075cd3ca3894a
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-07-28 14:34:02 -0300

    Configuração de rotas criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./routes";
 
 function App() {
 
   return (
-      <div>
-        <h1>Início</h1>
-      </div>
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
   )
 }
 
