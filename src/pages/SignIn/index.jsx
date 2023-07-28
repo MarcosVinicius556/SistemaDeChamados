f106020ca8976db6b82e316601a7771c87d97dd9
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-07-28 14:51:01 -0300

    Página de login estilizada

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,8 +1,38 @@
+import './signin.css'
+import logo from '../../assets/logo.png'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function SignIn() {
+
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
     return(
-        <div>
-            <h1>Página de SignIn</h1>
+        <div className='container-center'>
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt='Logo do sistema de chamados'/>
+                </div>
+                <form>
+                    <h1>Entrar</h1>
+                    <input 
+                        type="email" 
+                        placeholder='email@email.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input 
+                        type="password" 
+                        placeholder='***********'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                    <button type="submit">Acessar</button>
+                </form>
+                <Link to='/register'>Criar uma conta</Link>
+            </div>
         </div>
     );
 }
