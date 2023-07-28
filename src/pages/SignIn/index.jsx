d962f5412873c002b7995feba7bddc97335dc76b
Marcos Vinicius <marcosvinicios4132@gmail.com>
2023-07-28 14:27:17 -0300

    Projeto configurado, pages SignIn e SignUp criadas

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,10 @@
+
+function SignIn() {
+    return(
+        <div>
+            <h1>Página de SignIn</h1>
+        </div>
+    );
+}
+
+export default SignIn;
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,10 @@
+
+function SignUp() {
+    return(
+        <div>
+            <h1>Página de SignUp</h1>
+        </div>
+    );
+}
+
+export default SignUp;
